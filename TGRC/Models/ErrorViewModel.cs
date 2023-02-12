407bfd3302304ea9f2ee6426b1d9a31a7fa5d841
James Cubbage <jscubbage@ucdavis.edu>
2023-02-11 22:30:41 -0800

    don't need ?

--- TGRC/Models/ErrorViewModel.cs
+++ TGRC/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace TGRC.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
