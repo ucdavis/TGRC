621dfe5a96c723c2f3eb10770410d0a44f42256f
James Cubbage <jscubbage@ucdavis.edu>
2023-02-18 00:01:55 -0800

    Missing this table from initial design call

---
+++ Models/Taxa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TGRC.Models
+{
+    public partial class Taxa
+    {
+        public string Taxon { get; set; }
+        public string CompleteName  { get; set; }
+        public string Label { get; set; }
+        public string Synonym { get; set; }
+        public string Authority { get; set; }
+        public string SpeciesAbbreviation { get; set; }
+        public string L { get; set; }
+
+        public string Display
+        {
+            get{
+                if(CompleteName == "")
+                {
+                    return "";
+                }
+                return $"{CompleteName} ({Synonym})";
+            }
+        }
+        
+    }
+}
