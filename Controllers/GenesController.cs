1ecfcb6185eaea7ebd590c5f3c7129dcea0110a5
James Cubbage <jscubbage@ucdavis.edu>
2023-02-21 11:28:12 -0800

    gene search half done

--- Controllers/GenesController.cs
+++ Controllers/GenesController.cs
@@ -14,13 +14,25 @@ public class GenesController : Controller
         _context = context;
     }
 
-    public async Task<IActionResult> Detail(string id)
+    public async Task<IActionResult> Detail(string id, string frame = "no")
     {
         var model = await _context.Genes
             .Include(g => g.Alleles).ThenInclude(a => a.PhenoTypeDetails).ThenInclude(d => d.Category)
             .Include(g => g.GeneAccessions.Where(a => a.Accession.Status == "Active"))
             .Include(g => g.GeneImages).ThenInclude(i => i.Image)
             .Where(g => g.Gene1==id).FirstOrDefaultAsync();
+        ViewBag.Frame = frame;
+        return View(model);
+    }
+
+    public async Task<IActionResult> Search(GeneSearchViewModel vm)
+    {
+        if(!vm.Search)
+        {
+            var freshModel = await GeneSearchViewModel.Create(_context, null);
+            return View(freshModel);
+        }
+        var model = await GeneSearchViewModel.Create(_context, vm);
         return View(model);
     }
 
---
+++ Models/ViewModels/GeneSearchViewModel.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TGRC.Models
+{   
+    
+    public class GeneSearchViewModel
+    {
+        public List<Gene> genes { get; set; }
+
+        public bool Search { get; set; }
+
+        public string SelectedGene { get; set; }
+        public List<string> GeneList { get; set; }
+        public string SelectedLocus { get; set; }
+        public List<string> LocusList { get; set; }
+        public string SelectedAllele { get; set; }
+        public List<string> AlleleList { get; set; }
+        public string SynonymToSearch { get; set; }
+        public string SelectedMarker { get; set; }
+        public List<string> MarkerList { get; set; }  
+
+        public GeneSearchViewModel() {
+            Search = false;
+        }
+        
+        public static async Task<GeneSearchViewModel> Create(TGRCContext _context, GeneSearchViewModel vm)
+        { 
+           var geneList = await _context.GenesAndAllelesInAccessions.Select(a => a.Gene).Distinct().OrderBy(a=>a).ToListAsync();
+           geneList.Insert(0,"");
+           var locusList = await _context.Genes.Where(g => g.LocusName != null).Select(a => a.LocusName).Distinct().OrderBy(a=>a).ToListAsync();
+           locusList.Insert(0,"");
+           var alleleList = await _context.GenesAndAlleles.Where(g => g.Allele != null).Select(a => a.Allele).Distinct().OrderBy(a=>a).ToListAsync();
+           alleleList.Insert(0,"");
+           var markerList = await _context.Genes.Where(g => g.MarkerType != null).Select(g => g.MarkerType).Distinct().OrderBy(a=>a).ToListAsync();
+           markerList.Insert(0,"");
+
+                              
+            if(vm != null)
+            {
+                var geneToFind = _context.Genes.AsQueryable();                
+                   
+                if(!string.IsNullOrWhiteSpace(vm.SelectedGene))
+                {
+                    geneToFind = geneToFind.Where(g => g.Gene1 == vm.SelectedGene);
+                }
+                if(!string.IsNullOrWhiteSpace(vm.SelectedLocus))
+                {
+                    geneToFind = geneToFind.Where(g => g.LocusName == vm.SelectedLocus);
+                }
+                if(!string.IsNullOrWhiteSpace(vm.SelectedAllele))
+                {
+                    geneToFind = geneToFind.Where(g => g.Alleles.Any(a => a.Allele == vm.SelectedAllele));
+                }
+                if(!string.IsNullOrWhiteSpace(vm.SynonymToSearch))
+                {
+                    geneToFind = geneToFind.Where(g => g.Alleles.Any(a => EF.Functions.Like(a.SynonymsOfAllele, "%" + vm.SynonymToSearch + "%")));
+                }
+                if(!string.IsNullOrWhiteSpace(vm.SelectedMarker))
+                {
+                    geneToFind = geneToFind.Where(g => g.MarkerType == vm.SelectedMarker);
+                }
+                
+                
+                var viewModel = new GeneSearchViewModel
+                {
+                    genes = await geneToFind.ToListAsync(),                    
+                    SelectedGene = vm.SelectedGene,
+                    GeneList = geneList,
+                    LocusList = locusList,
+                    AlleleList = alleleList,
+                    MarkerList = markerList,
+                    SelectedLocus = vm.SelectedLocus,
+                    SelectedAllele = vm.SelectedAllele,
+                    SelectedMarker = vm.SelectedMarker,
+                    SynonymToSearch = vm.SynonymToSearch
+                };  
+                return viewModel;
+
+
+            }
+            var freshModel = new GeneSearchViewModel
+            {
+                genes = new List<Gene>(),
+                GeneList = geneList,
+                LocusList = locusList,
+                AlleleList = alleleList,
+                MarkerList = markerList
+            };           
+
+            return freshModel;
+        }
+
+        
+    } 
+    
+}
