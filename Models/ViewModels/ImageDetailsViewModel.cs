cc6b51901df034f00ceac80f012b2902a174fed6
James Cubbage <jscubbage@ucdavis.edu>
2023-03-17 22:13:41 -0700

    pull from accinimages => genesInAcc

--- Models/ViewModels/ImageDetailsViewModel.cs
+++ Models/ViewModels/ImageDetailsViewModel.cs
@@ -16,20 +16,22 @@ namespace TGRC.Models
 
         public List<AccessionsInImage> accession { get; set; }
 
-        public List<GenesAndAllelesInImage> genes { get; set; }
+        public List<GenesAndAllelesInAccession> genes { get; set; }
+        public List<ColleaguesInImage> colleagues { get; set; }
         public string taxon { get; set; }       
         
                
         public static async Task<ImageDetailsViewModel> Create(TGRCContext _context, string id)
         { 
             var imageEntry = await _context.Images.Where(i => i.File == id).FirstOrDefaultAsync();
-            var acc = await _context.AccessionsInImages.Where(i => i.ImageNum == imageEntry.ImageNum).ToListAsync();
+            var acc = await _context.AccessionsInImages.Where(i => i.ImageNum == imageEntry.ImageNum).OrderBy(a => a.AccessionNum).ToListAsync();
             var model = new ImageDetailsViewModel
             {
                 image = imageEntry,
                 accession = acc,
-                genes = await _context.GenesAndAllelesInImages.Where(i => i.ImageNum == imageEntry.ImageNum).ToListAsync(),
-                taxon = await _context.Accessions.Where(a => a.AccessionNum == acc.First().AccessionNum).Select(a => a.Taxon2).FirstOrDefaultAsync()
+                genes = await _context.GenesAndAllelesInAccessions.Where(g => acc.Select(a => a.AccessionNum).Distinct().ToList().Contains(g.AccessionNum)).ToListAsync(),
+                taxon = await _context.Accessions.Where(a => a.AccessionNum == acc.First().AccessionNum).Select(a => a.Taxon2).FirstOrDefaultAsync(),
+                colleagues = await _context.ColleaguesInImages.Where(c => c.ImageNum == imageEntry.ImageNum).Include(c => c.Colleague).ToListAsync(),
             };           
             
             return model;
